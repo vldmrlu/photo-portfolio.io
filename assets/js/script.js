d0cc0feac191344a682c3cb2358cfa58eaa79a8d
vldmrlu <lu.vldmr@gmail.com>
2022-08-14 03:56:52 +0300

    AOS fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,10 @@ const portfolioBlock = document.querySelector('.portfolio');
 if (portfolioBlock) {
   AOS.init();
 }
+
+document.querySelectorAll('.portfolio-img')
+  .forEach( (img) => 
+    img.addEventListener('load' , () => 
+      AOS.refresh()
+      )
+  );
