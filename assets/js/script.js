19fc77d688a2a05db6b3e0146cef5e00277fc6d9
vldmrlu <lu.vldmr@gmail.com>
2022-09-13 13:33:05 +0300

    ver beta
    new comm

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,23 @@
-const portfolioBlock = document.querySelector('.portfolio');
+let portfolioBlock = document.getElementById('portfolioBlock');
 
-document.addEventListener("DOMContentLoaded", function() {
-  if (portfolioBlock) {
-    AOS.init();
+if (portfolioBlock) {
+  AOS.init();
+}
+
+let burger = document.getElementById('burger');
+
+function menuClose(e) {
+  let { target } = e;
+  let menu = document.getElementById('burger');
+  let closeBtn = document.getElementById('close-burger');
+  let el = document.getElementsByClassName('offcanvas-backdrop')[0];
+  if ((e.target == closeBtn) || (e.target == el)) {
+    menu.classList.remove('active');
   }
-});
+}
+
+document.addEventListener('click', menuClose );
+
+burger.addEventListener('click', function () {
+  this.classList.add('active')
+})
